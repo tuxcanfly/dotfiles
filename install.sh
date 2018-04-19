9eff52709a3466623803e471ada2970a1770cf0a
Javed Khan <tuxcanfly@gmail.com>
2018-04-19 05:43:55 +0530

    WIP: goodies

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo pacman -S i3-wm rxvt-unicode dmenu neovim
+sudo pacman -S dmenu ranger
+sudo pacman -S feh imagemagick python-pip
+sudo pacman -S cairo libxcb python2 xcb-proto xcb-util-image xcb-util-wm xcb-util-xrm jsoncpp
+sudo pacman -S fontconfig freetype2
+sudo pacman -S imagemagick feh xorg-xrandr xorg-xdpyinfo
+sudo pacman -S dialog w3m compton i3-wm dunst i3lock i3status xsel rofi viewnior alsa-utils conky
+sudo pacman -S xprop xorg-xprop scrot mpd mpc terminus-font weechat bitlbee
+pacaur -S aur/i3lock-color lemonbar-git ttf-terminess nerd-font-complete base16-manager-git fzf-git
+pacaur -S polybar hsetroot fonts-noto suckless-tools ttf-font-icons betterlockscreen
