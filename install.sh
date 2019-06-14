377b49f16509f080e124b1471f815f19753f6df0
Javed Khan <tuxcanfly@gmail.com>
2019-06-14 23:56:42 +0530

    yay

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ sudo pacman -S imagemagick feh xorg-xrandr xorg-xdpyinfo
 sudo pacman -S dialog w3m compton i3-wm dunst i3lock i3status xsel rofi viewnior alsa-utils conky
 sudo pacman -S xprop xorg-xprop scrot mpd mpc terminus-font weechat bitlbee
 pacaur -S aur/i3lock-color lemonbar-git ttf-terminess nerd-font-complete base16-manager-git fzf-git
-pacaur -S polybar hsetroot fonts-noto suckless-tools ttf-font-icons betterlockscreen
+pacaur -S polybar hsetroot fonts-noto suckless-tools ttf-font-icons betterlockscreen yay-bin
