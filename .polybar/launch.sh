48dde31da38a67902e5087469f1d698be89bb33b
Javed Khan <tuxcanfly@gmail.com>
2019-12-22 21:33:29 +0530

    misc

--- .polybar/launch.sh
+++ .polybar/launch.sh
@@ -4,10 +4,10 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
-# Bar is the name set in the polybar config, so if you change it, you have to change it here too.
-polybar bar
+# Launch bar1 and bar2
+sleep 1 && polybar -c ~/.config/polybar/config -r default &
+
 
 echo "Bars launched..."
